4aba25e4360eebc33ebc2168fffa4f5de8a1eafc
carefulsuper <3082956466@qq.com>
2022-10-25 10:21:28 +0800

    如何让append和delete跟随左侧文字一起上下滚动？

--- src/components/TreeView.vue
+++ src/components/TreeView.vue
@@ -1,92 +1,121 @@
 <template>
-  <div class="home">
+  <div id="TreeView">
     <el-tree
-      :props="props"
-      :load="loadNode"
-      lazy
-      show-checkbox
-      @check-change="handleCheckChange"
+            :data="data"
+            node-key="id"
+            default-expand-all
+            :expand-on-click-node="false"
+    style="width: 300px;overflow: auto;height: 100px"
     >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span class="iconTwo">
+          <el-button
+                  type="text"
+                  size="mini"
+                  @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button
+                  type="text"
+                  size="mini"
+                  @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+      </span>
     </el-tree>
   </div>
 </template>
 
 <script>
-export default {
-  name: "TreeView",
-  components: {},
-  props: {},
-  data() {
-    return {
-      props: {
-        label: "name",
-        children: "zones",
-      },
-      count: 1,
-    };
-  },
-  methods: {
-    showOrNo() {
-      var hasChild;
-      if (hasChild) {
-        this.disabled = true;
+  let id = 1000;
+
+  export default {
+    data() {
+      const data = [{
+        id: 1,
+        label: '一级 1',
+        children: [{
+          id: 4,
+          label: '二级 1-1',
+          children: [{
+            id: 9,
+            label: '三级 1-1-1'
+          }, {
+            id: 10,
+            label: '三级 1-1-2'
+          }]
+        }]
+      }, {
+        id: 2,
+        label: '一级 2',
+        children: [{
+          id: 5,
+          label: '二级 2-1'
+        }, {
+          id: 6,
+          label: '二级 2-2'
+        }]
+      }, {
+        id: 3,
+        label: '一级 3',
+        children: [{
+          id: 7,
+          label: '二级 3-1'
+        }, {
+          id: 8,
+          label: '二级 3-2 派大星是海绵宝宝最好的朋友'
+        },
+          {
+            id: 11,
+            label: '二级 3-2 派大星是海绵宝宝最好的朋友'
+          },
+          {
+            id: 12,
+            label: '二级 3-2 派大星是海绵宝宝最好的朋友'
+          },
+          {
+            id: 13,
+            label: '二级 3-2 派大星是海绵宝宝最好的朋友'
+          }
+        ]
+      }];
+      return {
+        data
       }
     },
 
-    handleCheckChange(data, checked, indeterminate) {
-      console.log(data, checked, indeterminate);
-    },
-    handleNodeClick(data) {
-      console.log(data);
-    },
-    loadNode(node, resolve) {
-      if (node.level === 0) {
-        return resolve([{ name: "region1" }, { name: "region2" }]);
-      }
-      if (node.level > 3) return resolve([]);
-
-      var hasChild;
-      if (node.data.name === "region1") {
-        hasChild = true;
-      } else if (node.data.name === "region2") {
-        hasChild = false;
-      } else {
-        hasChild = Math.random() > 0.5;
-      }
-
-      setTimeout(() => {
-        var data;
-        if (hasChild) {
-          data = [
-            {
-              name: "zone" + this.count++,
-            },
-            {
-              name: "zone" + this.count++,
-            },
-          ];
-        } else {
-          data = [];
+    methods: {
+      append(data) {
+        const newChild = { id: id++, label: 'testtest', children: [] };
+        if (!data.children) {
+          this.$set(data, 'children', []);
         }
+        data.children.push(newChild);
+      },
 
-        resolve(data);
-      }, 500);
-    },
-  },
-};
+      remove(node, data) {
+        const parent = node.parent;
+        const children = parent.data.children || parent.data;
+        const index = children.findIndex(d => d.id === data.id);
+        children.splice(index, 1);
+      }
+    }
+  };
 </script>
 
-<style lang="scss">
-.el-tree-node__content {
-  position: relative;
-}
-.disabled {
-  cursor: not-allowed;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 2;
-}
+<style>
+  .custom-tree-node {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    padding-right: 8px;
+  }
+  .iconTwo{
+    position: fixed;
+    left: 200px;
+  }
 </style>
